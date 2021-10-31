1c171f2f76f03834960b1223e8ca3d08956937f2
Federico Hermida <fede.hermida@hotmail.com>
2021-10-31 10:46:17 -0300

    Update mongo connection string with environment variables

--- src/database/mongoConnection.js
+++ src/database/mongoConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/truenorth')
+mongoose.connect(`mongodb://${process.env.MONGOHOST}:${process.env.MONGOPORT}/truenorth`)
     .then(db => console.log('Connection Success', db))
     .catch(err => console.log(err))
 
